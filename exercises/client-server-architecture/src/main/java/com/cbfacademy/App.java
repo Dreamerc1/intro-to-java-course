28cb3a6ba3bd52221c9de52a519559281c8c325a
Dreamerc1 <chelsreid99@gmail.com>
2024-08-20 19:02:59 +0100

    Merging with new additions

--- exercises/client-server-architecture/src/main/java/com/cbfacademy/App.java
+++ exercises/client-server-architecture/src/main/java/com/cbfacademy/App.java
@@ -1,29 +1,32 @@
 package com.cbfacademy;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
+
+import java.net.URI;
 import java.net.URL;
-import java.net.URLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        //System.out.println("Hello World!");
-    try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-        URL myURL = new URL ("https://codingblackfemales.com/academy");
-        URLConnection myURLConnection = myURL.openConnection();
-        myURLConnection.connect();
-        String contentType = myURLConnection.getContentType();
-        System.out.println(contentType);
-    }
-    catch (MalformedURLException e){
-        System.out.println(e.getMessage());
-    }
-    catch (IOException e){
-        System.out.println(e.getMessage());
-    }
+    public static void main(String[] args) {
+        try {
+            URL url = new URI("https://codingblackfemales.com/").toURL();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-    
-    
+            connection.setRequestMethod("GET");
+
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String inputLine;
+
+                while ((inputLine = in.readLine()) != null) {
+                    System.out.println(inputLine);
+                }
+
+                // Closing the connection
+                connection.disconnect();
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
