a9c4d837fd126ddf004b0731bc1d77a13f91cd22
Dreamerc1 <chelsreid99@gmail.com>
2024-08-22 19:54:45 +0100

    Commiting software dev practise

---
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
@@ -0,0 +1,18 @@
+package com.cbfacademy;
+
+public class FizzBuzz {
+ public static String get(int number){
+    if(number % 15 == 0){
+        return "FizzBuzz";
+    }
+    else if (number % 5 == 0){
+    return "Buzz";
+    }
+    else if (number % 3 == 0){
+        return "Fizz";
+    }
+    else
+   // return number.toString();
+   return String.valueOf(number);
+ }
+}
--- exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
@@ -1,20 +1,40 @@
 package com.cbfacademy;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-
 @DisplayName(value = "Basic Test Suite")
 public class AppTest {
-
     @Test
     @DisplayName("creates the app")
     public void createsAnApp() {
         final App app = new App();
-
         assertThat(app, is(notNullValue()));
     }
+    @Test
+    @DisplayName("prints Fizz")
+    public void testFizz() {
+        FizzBuzz FizzBuzz = new FizzBuzz();
+        assertEquals("Fizz", FizzBuzz.get(3));
+        assertEquals("Fizz", FizzBuzz.get(6));
+        assertEquals("Fizz", FizzBuzz.get(9));
+        assertEquals("Fizz", FizzBuzz.get(12));
+    }
+    @Test
+    @DisplayName("prints Buzz")
+    public void testBuzz() {
+        assertEquals("Buzz", FizzBuzz.get(5));
+        assertEquals("Buzz", FizzBuzz.get(10));
+    }
+    @Test
+    @DisplayName("prints FizzBuzz")
+    public void testFizzBuzz() {
+        assertEquals("FizzBuzz", FizzBuzz.get(15));
+    }
 }
+// assertEquals("Fizz", FizzBuzz.printFizzBuzz(3));
+        // assertEquals("Buzz", FizzBuzz.printFizzBuzz(5));
+        // assertEquals("FizzBuzz", FizzBuzz.printFizzBuzz(15));
