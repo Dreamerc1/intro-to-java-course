e8f9527b64955717ba89d2cb03927efb10d4a89b
Dreamerc1 <chelsreid99@gmail.com>
2024-08-23 21:06:17 +0100

    Commiting new file - bank project

--- exercises/java-collections/src/main/java/com/cbfacademy/CollectionsExercises.java
+++ exercises/java-collections/src/main/java/com/cbfacademy/CollectionsExercises.java
@@ -4,27 +4,30 @@ import java.util.*;
 
 public class CollectionsExercises {
 
-    public LinkedList<Integer> useLinkedList() {
-        // TODO: create an empty linked list
-        //  - add 4 as the first element of the list
-        //  - then add 5, 6, 8, 2, 9 to the list
-        //  - add another 2 as the last element of the list
-        //  - add 4 as the 3rd element of the list
-        //  - invoke the method element() on the list and print the result on the screen
-        //  - return the list
-        //throw new RuntimeException("Not implemented");
-        LinkedList<Integer> integers = new LinkedList<>();
-        integers.addFirst(4);
-        integers.add(5);
-        integers.add(6);
-        integers.add(8);
-        integers.add(2);
-        integers.add(9);
-        integers.addLast(2);
-        integers.set(2,4);
-        System.out.println(integers.element());
-        return integers;
+    @SuppressWarnings("unchecked")
+    public HashMap<Integer, String> useHashMap() {
+        Map<Integer, String> hM = new HashMap<>();   // TODO: create an empty hash map (key value pairs)
+        hM.put(1, "TypeScript");//  - add {1, TypeScript} entry to the map
+        hM.put(2, "Kotlin"); //  - add {2, Kotlin} entry to the map
+        hM.put(3, "Python");//  - add {3, Python} entry to the map
+        hM.put(4, "Java");//  - add {4, Java} entry to the map
+        hM.put(5, "JavaScript");//  - add {5, JavaScript} entry to the map
+        hM.put(6, "Rust");//  - add {6, Rust} entry to the map
+        System.out.println(hM.keySet()); //  - determine the set of keys from the map and print it on the screen
+        System.out.println(hM.values());//  - determine the set of values from the map and print it on the screen
+        System.out.print(hM.containsValue("English"));//  - determine whether the map contains "English" as a language and print the result on the screen
+        return (HashMap<Integer, String>) hM.entrySet(); //  - return the map; initially I just put return myMap.entrySet()
+    }
 
+
+    public ArrayDeque<Integer> useArrayDeque() {
+        ArrayDeque<Integer> de = new ArrayDeque<>(); // TODO: create an empty arrayDeque
+        de.addAll(Arrays.asList(5, 6, 8, 9)); //  - add 5, 6, 8, 9 to the queue. Deque (1) = double ended queue and Dequeue (2) = to remove an element from the queue. You can re-write this so the first and second line are combined.
+        System.out.print(de.getFirst()); //  - print the first element of the queue on the screen
+        System.out.print(de.getLast()); //  - print the last element of the queue on the screen
+        System.out.print(de.poll()); //  - invoke the method poll() on the queue and print the result on the screen
+        System.out.print(de.element());  //  - invoke the element() method on the queue and print the result on the screen
+        return de;//  - return the queue
     }
 
     public Stack<Integer> useStack() {
@@ -49,54 +52,28 @@ public class CollectionsExercises {
         return stackElement;
 
     }
-
-    public ArrayDeque<Integer> useArrayDeque() {
-        // TODO: create an empty arrayDeque
-        ArrayDeque<Integer> de = new ArrayDeque<>();
-        //  - add 5, 6, 8, 9 to the queue
-        de.addFirst(5);
-        de.add(6);
-        de.add(8);
-        de.add(9);
-        //  - print the first element of the queue on the screen
-        //  - print the last element of the queue on the screen
-        System.out.println(de.getFirst());
-        System.out.println(de.getLast());
-        //  - invoke the method poll() on the queue and print the result on the screen
-        System.out.print(de.poll());
-        System.out.println(de.element());
-        return de;
-        //  - invoke the element() method on the queue and print the result on the screen
-        //  - return the queue
+    public LinkedList<Integer> useLinkedList() {
+        // TODO: create an empty linked list
+        //  - add 4 as the first element of the list
+        //  - then add 5, 6, 8, 2, 9 to the list
+        //  - add another 2 as the last element of the list
+        //  - add 4 as the 3rd element of the list
+        //  - invoke the method element() on the list and print the result on the screen
+        //  - return the list
         //throw new RuntimeException("Not implemented");
-    }
+        LinkedList<Integer> integers = new LinkedList<>();
+        integers.addFirst(4);
+        integers.add(5);
+        integers.add(6);
+        integers.add(8);
+        integers.add(2);
+        integers.add(9);
+        integers.addLast(2);
+        integers.set(2,4);
+        System.out.println(integers.element());
+        return integers;
 
-    public HashMap<Integer, String> useHashMap() {
-        // TODO: create an empty hash map
-        HashMap<Integer,String> hm = new HashMap<>();
-        //  - add {1, TypeScript} entry to the map
-        hm.put(1,"TypeScript");
-        //  - add {2, Kotlin} entry to the map
-        hm.put(2,"Kotlin");
-        //  - add {3, Python} entry to the map
-        hm.put(3, "Python");
-        //  - add {4, Java} entry to the map
-        hm.put(4, "Java");
-        //  - add {5, JavaScript} entry to the map
-        hm.put(5, "JavaScript");
-        //  - add {6, Rust} entry to the map
-        hm.put(6, "Rust");
-        //  - determine the set of keys from the map and print it on the screen
-        System.out.println(hm.keySet());
-        //  - determine the set of values from the map and print it on the screen
-        System.out.println(hm.values());
-        //  - determine whether the map contains "English" as a language and print the result on the screen
-        System.out.println(hm.containsValue("English"));
-        //  - return the map
-        return hm;
-        //throw new RuntimeException("Not implemented");
     }
-
     public String getName() {
         return "Collections Exercises";
     }
